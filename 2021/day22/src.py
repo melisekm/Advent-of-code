f3df5f11433266e465ab7bf402de1c3c3ed5742a
melisekm <46221266+melisekm@users.noreply.github.com>
2021-12-25 17:32:32 +0100

    day 21 pt1

---
+++ 2021/day22/src.py
@@ -0,0 +1,34 @@
+import timeit
+
+
+def load_input(file_name="in.txt"):
+    res = []
+    with open(file_name) as f:
+        for line in f:
+            res.append(line.strip())
+    return res
+
+
+def solve_pt1():
+    data = load_input()
+
+    pass
+
+
+def solve_pt2():
+    data = load_input()
+
+    pass
+
+
+start = timeit.default_timer()
+result1 = solve_pt1()
+end = timeit.default_timer()
+print(result1)
+print(f"Total time pt1: {(end - start):.3f} sec")
+
+# start = timeit.default_timer()
+# result2 = solve_pt2()
+# end = timeit.default_timer()
+# print(result2)
+# print(f"Total time pt2: {(end - start):.3f} sec")
