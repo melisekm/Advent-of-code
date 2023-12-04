6d231b3b2c215766f4223b281959649a264cfb4e
melisekm <melisekmartin1@gmail.com>
2023-12-04 19:44:51 +0100

    Day 04 - 2023 v3

--- 2023/04/src.py
+++ 2023/04/src.py
@@ -1,6 +1,5 @@
 import math
 import re
-from collections import defaultdict
 from functools import lru_cache
 
 from utils import aoc_part
@@ -10,39 +9,27 @@ def load_input(file_name="in.txt"):
     res = []
     with open(file_name, encoding='UTF-8') as f:
         for line in f:
-            line = line.strip().split(":")[1].split("|")
-            res.append(tuple(line))
+            winning, got = line.strip().split(":")[1].split("|")
+            res.append(sum(g in re.findall(r"\d+", winning) for g in re.findall(r"\d+", got)))
     return res
 
 
-@lru_cache(maxsize=None)
-def calc(_, card):
-    winning = re.findall(r"\d+", card[0])
-    got = re.findall(r"\d+", card[1])
-    return sum(g in winning for g in got)
+data = load_input()
 
 
 @aoc_part(1)
 def solve_pt1():
-    # calc for all, if calc is 0, then instead of 2^(-1) = 0.5 floor it to 0
-    return sum(math.floor(2 ** (calc(0, card) - 1)) for card in load_input())
+    return sum(math.floor(2 ** (card - 1)) for card in data)
 
 
-def card_cnt(idx, data, counts):
-    counts[idx + 1] += 1  # get real card id
-    winning_cnt = calc(idx, data[idx])  # calculate data for current card
-    # start at next card and go until <= winning_cnt
-    for i, _ in enumerate(data[idx + 1:idx + winning_cnt + 1], start=idx + 1):
-        card_cnt(i, data, counts)  # i == index of card in data
+@lru_cache(maxsize=None)
+def card_cnt(idx):
+    return 1 + sum(card_cnt(i) for i in range(idx + 1, idx + 1 + data[idx]))
 
 
 @aoc_part(2)
 def solve_pt2():
-    data = load_input()
-    counts = defaultdict(int)
-    for idx in range(len(data)):
-        card_cnt(idx, data, counts)
-    return sum(counts.values())
+    return sum(card_cnt(idx) for idx in range(len(data)))
 
 
 solve_pt1()
