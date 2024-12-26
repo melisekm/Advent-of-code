0e077ff13c46fee6c32d24839789c5005be1b361
melisekm <melisekmartin1@gmail.com>
2024-12-26 01:17:40 +0100

    Day 12 - 2024 - pt2

--- 2024/12/src.py
+++ 2024/12/src.py
@@ -1,5 +1,3 @@
-from collections import defaultdict
-
 import networkx as nx
 
 from utils import aoc_part
@@ -14,8 +12,9 @@ def load_input(file_name="in.txt"):
     return res
 
 
-def create_graph(data, R, C):
+def create_graph(data):
     G = nx.Graph()
+    R, C = len(data), len(data[0])
     for r in range(R):
         for c in range(C):
             val = data[r][c]
@@ -38,7 +37,7 @@ def create_graph(data, R, C):
 @aoc_part(1)
 def solve_pt1():
     data = load_input()
-    G = create_graph(data, len(data), len(data[0]))
+    G = create_graph(data)
     prices = 0
 
     for garden in nx.connected_components(G):
@@ -52,11 +51,59 @@ def solve_pt1():
     return prices
 
 
+def count_sides(garden: list[tuple[int, int]], dy, dx, next_node_dy, next_node_dx):
+    """
+    dy dx - we look at the direction to the side
+        ex: -1, 0 means we look up, if we don't see a node in garden, then this is a side
+    next_node_dy, next_node_dx - we continue in this direction and do the same
+        ex: 0, 1 means we go right and perform the same check
+    if we find a node that is not in garden, then this is the end of the side
+    """
+    seen = set()
+    count = 0
+    for r, c in garden:
+        if (r, c) in seen:
+            continue
+        seen.add((r, c))
+        # look to particular direction from node, if it is not in the garden, then this node is a part of new side
+        if (r + dy, c + dx) not in garden:
+            count += 1
+        else:
+            continue
+        # go until this side is finished, adding all that belong to it to seen, as those shouldn't be counted twice
+        while True:
+            next_node = (r + next_node_dy, c + next_node_dx)
+            if next_node not in garden:
+                break
+
+            above_node = (next_node[0] + dy, next_node[1] + dx)
+            if above_node not in garden:
+                seen.add(next_node)
+            else:
+                break
+            r += next_node_dy
+            c += next_node_dx
+    return count
+
+
+def sum_sides(garden: list[tuple[int, int]]):
+    sides = 0
+    sides += count_sides(garden, -1, 0, 0, 1)  # up right
+    sides += count_sides(garden, 0, -1, 1, 0)  # left, down
+    sides += count_sides(garden, 1, 0, 0, 1)  # down, right
+    sides += count_sides(garden, 0, 1, 1, 0)  # right, up
+    return sides
+
+
 @aoc_part(2)
 def solve_pt2():
-    pass
+    return sum(
+        # has to be sorted, otherwise we would count some nodes on side multiple times
+        sum_sides(sorted(garden)) * len(garden)
+        for garden in nx.connected_components(create_graph(load_input()))
+    )
 
 
 if __name__ == '__main__':
     solve_pt1()
-    # solve_pt2()
+    solve_pt2()
